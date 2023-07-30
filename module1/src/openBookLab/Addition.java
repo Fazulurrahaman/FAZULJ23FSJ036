7c712837d46d3174898dc1d665372bf176cb8d3f
fazulur <fazulur@fazulur-Vostro-3546>
2023-07-30 18:53:07 +0530

    addition program commit.

---
+++ module1/src/openBookLab/Addition.java
@@ -0,0 +1,16 @@
+package openBookLab;
+
+/*Aim- java program to add two numbers*/
+public class Addition {
+
+	public static void main(String[] args) {
+	
+		int first =100;
+		int second =200;
+		System.out.println(first+" "+second);
+		//add two numbers
+		int sum =first+second;
+		System.out.println("The sum is: "+sum);
+	}
+
+}
